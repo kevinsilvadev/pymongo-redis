9fc23969aa5fa7e2baf4b925a9f47356e26893e2
kevinsilvadev <kivim2018@gmail.com>
2023-03-24 21:59:50 -0300

    Last commit finished

--- project-redis-pymongo/BD/Connection.py
+++ project-redis-pymongo/BD/Connection.py
@@ -1,10 +1,54 @@
 import pymongo as pm
+import redis as rd
+import json as j
 
 class Connection:
 
-    def connection():
-        mongoClient = pm.MongoClient("mongodb+srv://mongodb:<password>@cluster0.biiuuzo.mongodb.net/?retryWrites=true&w=majority")
+    @staticmethod
+    def connectionMongoDb():
+        # ----------------- Connection MongoDB ------------------------#
+        mongoClient = pm.MongoClient(
+            "mongodb+srv://mongodb:<password>@cluster0.biiuuzo.mongodb.net/?retryWrites=true&w=majority")
         bd = mongoClient.vestibular
         coluna = bd.aprovados
-        coluna.drop
-    
+       # coluna.drop()
+        
+        # ------------------- Connection Redis --------------------------#
+        redis_client = rd.Redis(host="localhost", port=6379)
+        
+        # Sincronização inicial de dados
+        for doc in coluna.find():
+            key = str(doc["_id"])
+            redis_client.set(key, j.dumps(doc, default=str))
+
+            # Monitoramento de mudanças no MongoDB
+        with coluna.watch(full_document="updateLookup") as change_stream:
+            print("Entrou aqui")
+            for change in change_stream:
+                print("Entrou no for")
+                print(change_stream)
+                operation_type = change["operationType"]
+                print(
+                    f"\n\n{'=-'*75}\nOperation type executed: {operation_type}")
+
+                if operation_type in ["insert", "update"]:
+                    full_document = change["fullDocument"]
+                    print(f"Full Document: {full_document}")
+
+                    document_json = j.dumps(full_document, default=str)
+                    key = str(full_document["_id"])
+                    redis_client.set(key, document_json)
+                    doc = redis_client.get(key)
+                    print(f"Document in Redis: {doc}\n{'=-'*75}")
+                    print(
+                        f"Document {key} insert or update in Redis with sucess!")
+
+                elif operation_type == "delete":
+                    key = str(change['documentKey']['_id'])
+                    result = redis_client.delete(key)
+                    if result == 0:
+                        print("Document not for removal")
+                    else:
+                        print(
+                            f"\nDocument {key} deleted Redis\n{'=-'*75}")
+        return coluna
--- project-redis-pymongo/Program.py
+++ project-redis-pymongo/Program.py
@@ -5,24 +5,24 @@ from BD.Connection import Connection
 
 def main():
     def generatorPerson():
-            fake = Faker("pt_BR")
-            courses = ['Engenharia de Software', 'Engenharia da Computação', 'Arquitetura', 'Física', 'Educação Física', 'Ciencia da Computação']
-            years = [2023, 2024]
-            approved_list = []
-            for i in range(5000):
-                name = fake.name()
-                cpf = fake.cpf()
-                years = random.choice(years)
-                aproved_course = random.choice(courses)
-                Person(name, cpf, years, aproved_course)
-                candidate = {
-                     'name': name,
-                     'cpf': cpf,
-                     'years': years,
-                     'cursos_aprovados': aproved_course
-                }
-                approved_list.append(candidate)
-            Connection.connection().insert_many(approved_list)
+        fake = Faker("pt_BR")
+        courses = ['Engenharia de Software', 'Engenharia da Computação',
+                   'Arquitetura', 'Física', 'Educação Física', 'Ciencia da Computação']
+        years = [2023, 2024]
+        approved_list = []
+        for i in range(5000):
+            name = fake.name()
+            cpf = fake.cpf()
+            year = random.choice(years)
+            aproved_course = random.choice(courses)
+            Person(fake.name(), cpf, year, aproved_course)
+            candidate = {
+                'name': name,
+                'cpf': cpf,
+                'years': year,
+                'cursos_aprovados': aproved_course
+            }
+            approved_list.append(candidate)
+        Connection.connectionMongoDb().insert_many(approved_list)
     generatorPerson()
 main()
-                
